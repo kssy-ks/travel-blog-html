5ce4fbf7695bcd2fe8757ff428e824704eaa418a
ksenia <aksinyacs@icloud.com>
2023-03-23 00:28:36 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const navBtn=document.querySelector('.nav-button');
+const mobileNav=document.querySelector('.mobile-nav');
+const body = document.body;
+
+
+ navBtn.addEventListener('click', function(event){
+    event.stopPropagation();
+    toggleMobileNav();
+    
+ })
+
+ window.addEventListener('click', function(){
+    if(body.classList.contains('no-scroll')){
+        toggleMobileNav();
+    }
+ })
+
+ mobileNev.addEventListener('click', function(event){
+    event.stopPropagation();
+ })
+
+ function toggleMobileNav(){
+    mobileNav.classList.toggle('mobile-nav-active');
+    navBtn.classList.toggle('nav-button-close');
+    body.classList.toggle('no-scroll');
+
+ }
